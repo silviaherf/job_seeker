1fa2f1da51ac007ca49c5f2b629b9aaea00000fa
Silvia Hernandez <silviaherf@gmail.com>
2020-11-05 16:32:15 +0100

    sin apertura ventana chrome

--- src/data_extraction.py
+++ src/data_extraction.py
@@ -2,17 +2,25 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
+import re
 import os
 from dotenv import load_dotenv
 load_dotenv(dotenv_path='./.env')
+from pyvirtualdisplay import Display
 
 
 
 
 def jobs_scrapping(key='data',city='Madrid'):
 
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--window-size=1420,1080')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+
     CHROME_PATH = os.getenv("CHROME_PATH")
-    driver = webdriver.Chrome(CHROME_PATH)
+    driver = webdriver.Chrome(CHROME_PATH, chrome_options=chrome_options)
     driver.get('https://www.linkedin.com/login/es?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin')
     time.sleep(2)
  
@@ -26,25 +34,37 @@ def jobs_scrapping(key='data',city='Madrid'):
     button=driver.find_element(By.XPATH,'//*[@id="app__container"]/main/div[3]/form/div[3]/button')
     time.sleep(0.5)
     button.click()
-    time.sleep(1)
+    time.sleep(0.5)
 
 
     jobs=driver.find_element(By.XPATH,'//*[@id="ember26"]')
+    #jobs=driver.find_element_by_link_text('/jobs/')
     time.sleep(0.3)
     jobs.click()
     time.sleep(0.3)
 
-    #key=driver.find_element_by_class_name('jobs-search-box__text-input')
-    #key=driver.find_element_by_css_selector('input.jobs-search-box-keyword-id-ember644')
-    key=driver.find_element_by_css_selector(".jobs-search-box__text-input jobs-search-box__ghost-text-input[aria-label='Busca por cargo, aptitud o empresa']")
-    key.send_keys(key)
+    #key_find=driver.find_element_by_class_name('jobs-search-box__inner ember-view')
+    key_find=driver.find_element_by_id('//*[@id="ember1921"]')
+    #key_find=driver.find_element_by_xpath("//*[contains(@id,'jobs-search-box-keyword-id-ember')]")
+    """
+    <input id="jobs-search-box-keyword-id-ember1918" class="jobs-search-box__text-input" 
+    autocomplete="chrome-off" spellcheck="false" role="combobox" 
+    aria-autocomplete="list" aria-activedescendant="" 
+    aria-expanded="false" aria-owns="" type="text">
+   """
+    #key_find=driver.find_element_by_css_selector(".jobs-search-box__text-input[aria-label='Busca por cargo, aptitud o empresa']")
+    key_find.send_keys(key)
     time.sleep(0.3)
-    city=driver.find_element(By.XPATH,'//*[@id="jobs-search-box-location-id-ember621"]')
+    city=driver.find_element_by_xpath("//*[contains(@id,'jobs-search-box-location-id-ember')]")
+   
     city.send_keys(city)
+
     time.sleep(0.3)
     find=driver.find_element(By.CLASS_NAME,'jobs-search-box__submit-button artdeco-button artdeco-button--3 jobs-home-soho__mvp-button')
     time.sleep(0.3)
     find.click()
+
+
 
     
     #driver.close()
